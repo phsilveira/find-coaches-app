00d47098b926eec653009d9da954d0c27146fe7a
Paulo Henrique Silveira <paulo.silveira@gabriel.com.br>
2023-05-06 14:48:49 -0300

    adding base components (button, card, badge)

--- src/pages/coaches/CoachsList.vue
+++ src/pages/coaches/CoachsList.vue
@@ -1,16 +1,18 @@
 <template>
   <section>FILTER</section>
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as Coach</router-link>
-    </div>
-    <ul v-if="hasCoaches">
-      <li v-for="coach in filteredCoaches" :key="coach.id">
-        <CoachItem :coach="coach" />
-      </li>
-    </ul>
-    <h3 v-else>No coaches found.</h3>
+    <base-card>
+      <div class="controls">
+        <base-button mode="outline">Refresh</base-button>
+        <base-button link to="/register">Register as Coach</base-button>
+      </div>
+      <ul v-if="hasCoaches">
+        <li v-for="coach in filteredCoaches" :key="coach.id">
+          <CoachItem :coach="coach" />
+        </li>
+      </ul>
+      <h3 v-else>No coaches found.</h3>
+    </base-card>
   </section>
 </template>
 
